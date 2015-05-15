0afa705384f1b864c959b0988731015f59a58568
Piotr Niełacny <piotr.nielacny@gmail.com>
2015-05-15 15:20:44 +0200

    Use MimeMagic#by_extension to recognize file type

--- lib/paperclip/content_type_detector.rb
+++ lib/paperclip/content_type_detector.rb
@@ -68,7 +68,8 @@ module Paperclip
 
     def type_from_mime_magic
       @type_from_mime_magic ||=
-        MimeMagic.by_magic(File.open(@filepath)).try(:type)
+        MimeMagic.by_magic(File.open(@filepath)).try(:type) ||
+        MimeMagic.by_extension(File.extname(@filepath))
     end
 
     def type_from_file_command
